140493a1694c711cc3895fab504ab1bce9e3f21a
BoikoIlya <inmortal.ilya@yandex.by>
2023-01-25 14:35:35 +0300

    initial commit

---
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.musicapp
+
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaParser
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.musicapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+    }
+
+}
