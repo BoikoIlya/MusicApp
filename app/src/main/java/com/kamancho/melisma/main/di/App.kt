76a04e46fa3652efc79d0ca024c431e6af12db4f
BoikoIlya <inmortal.ilya@yandex.by>
2023-12-01 22:54:59 +0300

    add app metrica

--- app/src/main/java/com/kamancho/melisma/main/di/App.kt
+++ app/src/main/java/com/kamancho/melisma/main/di/App.kt
@@ -1,12 +1,18 @@
 package com.kamancho.melisma.main.di
 
 import android.app.Application
+import com.kamancho.melisma.BuildConfig
+import io.appmetrica.analytics.AppMetrica
+import io.appmetrica.analytics.AppMetricaConfig
 
 /**
  * Created by HP on 28.01.2023.
  **/
 class App: Application() {
 
+    companion object{
+        private const val API_KEY:String = "bf9e4443-af4b-4546-ae44-46bf0f0b61b0"
+    }
 
     lateinit var appComponent: AppComponent
 
@@ -14,5 +20,9 @@ class App: Application() {
     override fun onCreate() {
         appComponent = DaggerAppComponent.builder().context(this).build()
         super.onCreate()
+
+        if(BuildConfig.DEBUG) return
+        val config = AppMetricaConfig.newConfigBuilder(API_KEY).build()
+        AppMetrica.activate(this, config)
     }
 }
